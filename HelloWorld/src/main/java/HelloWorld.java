2756fb2a69fea32828cd1d6cde2c447ae534ed60
Daniela <Daniela@Danu>
2016-07-01 11:27:02 +0300

    New Class Util

---
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Util.printMessage("Hello World!");
+
+	}
+
+}
---
+++ HelloWorld/src/main/java/Util.java
@@ -0,0 +1,9 @@
+
+public class Util
+{
+	public static void printMessage(String message)
+	{
+		System.out.println(message);
+	}
+}
+
